b5a9535df734865e55b0382379a00fde21aba24f
Li Zhineng <im@zhineng.li>
2021-02-18 16:11:09 +0800

    wip

--- src/MnsQueue.php
+++ src/MnsQueue.php
@@ -4,39 +4,62 @@ namespace Zhineng\QueueMns;
 
 use AliyunMNS\Client;
 use AliyunMNS\Exception\MessageNotExistException;
+use AliyunMNS\Model\QueueAttributes;
 use AliyunMNS\Requests\SendMessageRequest;
-use Illuminate\Contracts\Queue\ClearableQueue;
 use Illuminate\Contracts\Queue\Queue as QueueContract;
 use Illuminate\Queue\Queue;
 
-class MnsQueue extends Queue implements QueueContract, ClearableQueue
+class MnsQueue extends Queue implements QueueContract
 {
     public function __construct(
-        protected $mns,
+        protected Client $mns,
         protected string $default,
         bool $dispatchAfterCommit = false
     ) {
         $this->dispatchAfterCommit = $dispatchAfterCommit;
     }
 
-    public function clear($queue)
-    {
-        // TODO: Implement clear() method.
-    }
-
+    /**
+     * @inheritDoc
+     */
     public function size($queue = null)
     {
-        // TODO: Implement size() method.
+        /** @var QueueAttributes $response */
+        $response = $this->mns
+            ->getQueueRef($this->getQueue($queue))
+            ->getAttribute()
+            ->getQueueAttributes();
+
+        return (int) $response->getActiveMessages()
+            + (int) $response->getInactiveMessages()
+            + (int) $response->getDelayMessages();
     }
 
+    /**
+     * @inheritDoc
+     */
     public function push($job, $data = '', $queue = null)
     {
-        // TODO: Implement push() method.
+        return $this->enqueueUsing(
+            $job,
+            $this->createPayload($job, $queue ?: $this->default, $data),
+            $queue,
+            null,
+            function ($payload, $queue) {
+                return $this->pushRaw($payload, $queue);
+            }
+        );
     }
 
+    /**
+     * @inheritDoc
+     */
     public function pushRaw($payload, $queue = null, array $options = [])
     {
-        // TODO: Implement pushRaw() method.
+        return $this->mns
+            ->getQueueRef($this->getQueue($queue))
+            ->sendMessage(new SendMessageRequest($payload))
+            ->getMessageId();
     }
 
     /**
@@ -77,8 +100,24 @@ class MnsQueue extends Queue implements QueueContract, ClearableQueue
         }
     }
 
+    /**
+     * Get the queue of return the default.
+     *
+     * @param  string|null  $queue
+     * @return string
+     */
     protected function getQueue(?string $queue = null): string
     {
         return $queue ?: $this->default;
+    }
+
+    /**
+     * Get the underlying MNS instance.
+     *
+     * @return \AliyunMNS\Client
+     */
+    public function getMns(): Client
+    {
+        return $this->mns;
     }
 }
--- tests/MnsQueueTest.php
+++ tests/MnsQueueTest.php
@@ -2,7 +2,9 @@
 
 namespace Zhineng\QueueMns\Tests;
 
+use AliyunMNS\Client;
 use AliyunMNS\Exception\MessageNotExistException;
+use AliyunMNS\Model\QueueAttributes;
 use Illuminate\Container\Container;
 use Illuminate\Support\Carbon;
 use Mockery as m;
@@ -22,13 +24,29 @@ class MnsQueueTest extends TestCase
 
     protected function setUp(): void
     {
-        $this->mns = m::mock(MnsQueue::class);
+        $this->mns = m::mock(Client::class);
 
         $this->queueName = 'emails';
 
         $this->mockedJob = 'foo';
         $this->mockedData = ['data'];
+        $this->mockedDelay = 10;
         $this->mockedMessageId = '5F290C926D472878-2-14D9529****-200000001';
+
+        $this->mockedQueueAttributesModel = new QueueAttributes(
+            '30',
+            '65536',
+            '65536',
+            '60',
+            '0',
+            $this->queueName,
+            '1250700999',
+            '1250700999',
+            '20',
+            '0',
+            '0',
+            true
+        );
     }
 
     public function testPopProperlyPopsJobOffOfMns()
@@ -51,15 +69,46 @@ class MnsQueueTest extends TestCase
         $this->assertNull($result);
     }
 
-    public function testDelayedPushWithDateTimeProperlyPushesJobOntoSqs()
+    public function testDelayedPushWithDateTimeProperlyPushesJobOntoMns()
     {
         $now = Carbon::now();
         $queue = new MnsQueue($this->mns, $this->queueName);
         $queue->setContainer($container = m::spy(Container::class));
         $this->mns->shouldReceive('getQueueRef')->once()->with($this->queueName)->andReturn($queueRef = m::mock(stdClass::class));
-        $queueRef->shouldReceive('sendMessage')->once()->withAnyArgs()->andReturn(new MockedSendMessageResponseModel);
+        $queueRef->shouldReceive('sendMessage')->once()->andReturn(new MockedSendMessageResponseModel);
         $id = $queue->later($now->addSeconds(5), $this->mockedJob, $this->mockedData, $this->queueName);
         $this->assertEquals($this->mockedMessageId, $id);
         $container->shouldHaveReceived('bound')->with('events')->once();
+    }
+
+    public function testDelayedPushProperlyPushesJobOntoMns()
+    {
+        $queue = new MnsQueue($this->mns, $this->queueName);
+        $queue->setContainer($container = m::spy(Container::class));
+        $this->mns->shouldReceive('getQueueRef')->once()->with($this->queueName)->andReturn($queueRef = m::mock(stdClass::class));
+        $queueRef->shouldReceive('sendMessage')->once()->andReturn(new MockedSendMessageResponseModel);
+        $id = $queue->later($this->mockedDelay, $this->mockedJob, $this->mockedData, $this->queueName);
+        $this->assertEquals($this->mockedMessageId, $id);
+        $container->shouldHaveReceived('bound')->with('events')->once();
+    }
+
+    public function testPushProperlyPushesJobOntoMns()
+    {
+        $queue = new MnsQueue($this->mns, $this->queueName);
+        $queue->setContainer($container = m::spy(Container::class));
+        $this->mns->shouldReceive('getQueueRef')->once()->with($this->queueName)->andReturn($queueRef = m::mock(stdClass::class));
+        $queueRef->shouldReceive('sendMessage')->once()->andReturn(new MockedSendMessageResponseModel);
+        $id = $queue->push($this->mockedJob, $this->mockedData, $this->queueName);
+        $this->assertEquals($this->mockedMessageId, $id);
+        $container->shouldHaveReceived('bound')->with('events')->once();
+    }
+
+    public function testSizeProperlyReadsMnsQueueSize()
+    {
+        $queue = new MnsQueue($this->mns, $this->queueName);
+        $this->mns->shouldReceive('getQueueRef')->once()->with($this->queueName)->andReturn($queueRef = m::mock(stdClass::class));
+        $queueRef->shouldReceive('getAttribute->getQueueAttributes')->once()->andReturn($this->mockedQueueAttributesModel);
+        $size = $queue->size($this->queueName);
+        $this->assertEquals(20, $size);
     }
 }
